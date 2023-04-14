c54244921f25f5bde4fe9a18275c7a27c2e5d92a
BigBen3 <benjamin.s.sandler@gmail.com>
2023-04-14 14:25:04 -0700

    o

---
+++ spot/src/routes/connect/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { authHandlers } from  '../store/store';
+    export let data: PageData;
+</script>
+
+
+<h1>Hello new user</h1>
+
+
+<a
+			href={data.spotifyAuthUrl}
+			class="px-4 py-2 rounded-full bg-[#1DB954] font-bold hover:bg-opacity-90 active:translate-y-1 transition-all duration-300"
+		>
+			Get started
+		</a>
+
+
+        <button on:click={authHandlers.logout}>
+            <p>Logout</p></button>

4687a6cb24a40a5f3ebb050ea0d5e4ec6643b88a
BigBen3 <benjamin.s.sandler@gmail.com>
2023-04-14 18:56:23 -0700

    image link

--- spot/src/routes/connect/+page.svelte
+++ spot/src/routes/connect/+page.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    //import { authHandlers } from  '../store/store';
-    export let data: PageData;
+	import type { PageData } from './$types';
+	//import { authHandlers } from  '../store/store';
+	export let data: PageData;
 </script>
-
 
 <h1>Hello new user</h1>
 
-
-<a
-			href={data.spotifyAuthUrl}
-			class="px-4 py-2 rounded-full bg-[#1DB954] font-bold hover:bg-opacity-90 active:translate-y-1 transition-all duration-300"
-		>
-			Get started
-		</a>
+<a href={data.spotifyAuthUrl}>
+	<img
+		src="https://play-lh.googleusercontent.com/eN0IexSzxpUDMfFtm-OyM-nNs44Y74Q3k51bxAMhTvrTnuA4OGnTi_fodN4cl-XxDQc"
+		alt="hello"
+		width="500"
+		height="500"
+	/>
+</a>
 
 <!-- <button on:click={authHandlers.logout}>
             <p>Logout</p></button>-->
-       

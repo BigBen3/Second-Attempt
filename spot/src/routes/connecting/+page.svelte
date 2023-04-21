628eb43b53a40c3d147268e12e714c1d59212994
BigBen3 <benjamin.s.sandler@gmail.com>
2023-04-21 07:51:38 -0700

    errors

---
+++ spot/src/routes/connecting/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { goto } from '$app/navigation';
+	// @ts-ignore
+	import { authStore } from '../store/store';
+
+	// data should contain access_token, token_type etc...
+	export let data;
+
+	$: finishAuth($authStore.user);
+
+	/**
+	 * @param {any} user
+	 */
+	async function finishAuth(user) {
+		if (user) {
+			// you can now save the data in the database
+			// then you can redirect the user to the page "dashboard"
+			goto('/dashboard');
+		}
+	}
+</script>
+
+<strong>Finishing authentication...</strong>
